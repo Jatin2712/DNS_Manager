595344f98de46e4fc8f43542cdc94e83a1fb7803
Jatin Bundel <58298647+Jatin2712@users.noreply.github.com>
2024-06-02 17:19:03 +0530

    URL Changed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://dns-manager-a549.onrender.com/api/',
   headers: {
     'Content-Type': 'application/json'
   }
